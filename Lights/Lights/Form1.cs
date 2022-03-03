49126dd57b9b1b6a85218e16e8b8e7d1d124be34
Categio <categio1@gmail.com>
2022-03-02 19:10:59 -0800

    created a light program to practice creating methods. Added functionality to the lightStateLabel to coincide with the button clicks.

---
+++ Lights/Lights/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lights
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TurnLightOn()
+        {
+            //display the light on image
+            lightOnPictureBox.Visible = true;
+
+            //hide light off image
+            lightOffPictureBox.Visible = false;
+
+            //update label under bulbs
+            lightStateLabel.Text = "ON";
+        }
+
+        private void TurnLightOff()
+        {
+            //hide light on image
+            lightOnPictureBox.Visible = false;
+
+            //display light off image
+            lightOffPictureBox.Visible = true;
+
+            //update label under bulbs
+            lightStateLabel.Text = "OFF";
+        }
+
+        private void switchButton_Click(object sender, EventArgs e)
+        {
+            //Display the proper image for the button click
+            if (lightOnPictureBox.Visible == true)
+            {
+                TurnLightOff();
+            }
+            else
+            {
+                TurnLightOn();
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
